2731d7ccece50f703fb60b0f024f91b112f1e2c4
chintan87 <chintankapdi1987@gmail.com>
2023-01-11 15:48:57 +0530

    Improving dark mode & refactoring App component

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -18,16 +18,37 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  const handleCopy = () => {
+    // console.log("I am coppy");
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/); // javaScript regexs
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
   // *> text = "new text"; // Wrong way to change the state
   // *> setText("new text"); // Correct way to change the state
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
             className="form-control"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
             id="myBox"
             value={text}
             onChange={handleOnChange}
@@ -40,15 +61,30 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={handleLoClick}>
           Convert to Lowercase
         </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Space
+        </button>
       </div>
-      <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
         <h2>Your text summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textobx above to preview it here"}
+        </p>
       </div>
     </>
   );
